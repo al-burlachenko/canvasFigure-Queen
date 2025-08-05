ec904d3ccad52a3461821bacec4367d3973a92aa
al-burlachenko <alexandrburl@gmail.com>
2025-08-05 10:35:15 +0300

    fixed version: changes logic and improves it

--- src/script.js
+++ src/script.js
@@ -3,13 +3,13 @@ function drawQueen() {
   const ctx = canvas.getContext("2d");
 
   const config = {
-    base: { x: 35, y: 160, width: 60, height: 10 },
-    secSeg: { width: 30, height: 10 },
-    thirdSeg: { widthBottom: 20, widthTop: 15, height: 40 },
-    botCross: { width: 45, height: 5, distance: 25 },
+    base: { x: 70, y: 160, width: 60, height: 10 },
+    secSeg: { width: 40, height: 10 },
+    thirdSeg: { widthBottom: 20, widthTop: 10, height: 40 },
+    botCross: { width: 50, height: 5, distance: 40 },
     topCross: { width: 25, height: 5, distance: 10 },
     shift: 5,
-    radius: 8,
+    radius: 12,
   };
 
   const angle = Math.atan(5 / (2 * config.radius));
@@ -17,122 +17,128 @@ function drawQueen() {
   ctx.beginPath();
   let x = config.base.x;
   let y = config.base.y;
-  let mirror = 1;
 
-  x += config.base.width / 2;
-  ctx.moveTo(x, y);
+  function drawHalfFigure(mirror) {
+    x = config.base.x;
+    y = config.base.y;
+    ctx.moveTo(x, y);
 
-  x += (config.base.width / 2) * mirror;
-  ctx.lineTo(x, y);
+    x += (config.base.width * mirror) / 2;
+    ctx.lineTo(x, y);
 
-  y -= config.base.height * mirror;
-  ctx.lineTo(x, y);
+    y -= config.base.height;
+    ctx.lineTo(x, y);
 
-  x -= config.base.width / 2 - config.secSeg.width / 2;
-  ctx.lineTo(x, y);
+    x -= (config.base.width * mirror) / 2 - (config.secSeg.width * mirror) / 2;
+    ctx.lineTo(x, y);
 
-  y -= config.secSeg.height * mirror;
-  ctx.lineTo(x, y);
+    y -= config.secSeg.height;
+    ctx.lineTo(x, y);
 
-  x -= (config.secSeg.width - config.thirdSeg.widthBottom) / 2;
-  ctx.lineTo(x, y);
+    x -=
+      (config.secSeg.width * mirror) / 2 -
+      (config.thirdSeg.widthBottom * mirror) / 2;
+    ctx.lineTo(x, y);
 
-  x -= (config.thirdSeg.widthBottom - config.thirdSeg.widthTop) / 2;
-  y -= config.thirdSeg.height * mirror;
-  ctx.lineTo(x, y);
+    x -=
+      (config.thirdSeg.widthBottom * mirror) / 2 -
+      (config.thirdSeg.widthTop * mirror) / 2;
+    y -= config.thirdSeg.height;
+    ctx.lineTo(x, y);
 
-  x -= config.thirdSeg.widthTop / 2 - config.botCross.width / 2;
-  ctx.lineTo(x, y);
+    x -=
+      (config.thirdSeg.widthTop * mirror) / 2 -
+      (config.botCross.width * mirror) / 2;
+    ctx.lineTo(x, y);
 
-  y -= config.botCross.height * mirror;
-  ctx.lineTo(x, y);
+    y -= config.botCross.height;
+    ctx.lineTo(x, y);
 
-  x -= config.botCross.width / 2 - config.shift / 2;
-  ctx.lineTo(x, y);
+    x -= (config.botCross.width * mirror) / 2 - (config.shift * mirror) / 2;
+    ctx.lineTo(x, y);
 
-  ctx.arc(
-    x - (config.shift * mirror) / 2,
-    y - (config.botCross.distance * mirror) / 2,
-    config.radius,
-    (Math.PI / 2) * mirror - angle,
-    (-Math.PI / 2) * mirror + angle,
-    true
-  );
-  y -= config.botCross.distance * mirror;
-  ctx.lineTo(x, y);
+    x -= (config.shift * mirror) / 2;
+    y -= config.botCross.distance / 2;
+    ctx.arc(
+      x,
+      y,
+      config.radius,
+      Math.PI / 2 - angle * mirror,
+      -Math.PI / 2 + angle * mirror,
+      mirror > -1
+    );
 
-  x -= config.shift / 2 - config.topCross.width / 2;
-  ctx.lineTo(x, y);
+    x += (config.shift * mirror) / 2;
+    y -= config.botCross.distance / 2;
+    ctx.lineTo(x, y);
 
-  y -= config.topCross.height * mirror;
-  ctx.lineTo(x, y);
+    x -= (config.shift * mirror) / 2 - (config.topCross.width * mirror) / 2;
+    ctx.lineTo(x, y);
 
-  x += config.shift / 2 - config.topCross.width / 2;
-  ctx.lineTo(x, y);
+    y -= config.topCross.height;
+    ctx.lineTo(x, y);
+    x -= (config.topCross.width * mirror) / 2 - (config.shift * mirror) / 2;
+    ctx.lineTo(x, y);
 
-  y -= config.topCross.distance * mirror;
-  ctx.lineTo(x, y);
+    y -= config.topCross.distance;
+    ctx.lineTo(x, y);
 
-  x -= config.shift / 2;
-  ctx.lineTo(x, y);
-  // !! // // !! //// !! //// !! //// !! //// !! //// !! //
-  mirror = -1;
-  x -= config.shift / 2;
-  ctx.lineTo(x, y);
+    x -= (config.shift * mirror) / 2;
+    ctx.lineTo(x, y);
+  }
 
-  y -= config.topCross.distance * mirror;
-  ctx.lineTo(x, y);
+  function drawCrown() {
+    ctx.beginPath();
+    const figureHeight =
+      config.base.height +
+      config.secSeg.height +
+      config.thirdSeg.height +
+      config.botCross.height +
+      config.botCross.distance +
+      config.topCross.height +
+      config.topCross.distance;
+    // ctx.strokeStyle = "aqua";
+    // ctx.fillStyle = "blue";
+    let x = config.base.x;
+    let y = config.base.y - figureHeight;
+    const crownWidth = 30;
+    const crownHeight = 10;
+    ctx.moveTo(x, y);
+    ctx.lineTo(x - crownWidth / 2, y - crownHeight);
+    ctx.lineTo(x + crownWidth / 2, y - crownHeight);
+    ctx.fill();
+    // ctx.rect(x - 15, y - 15, 5, -8);
 
-  x += config.shift / 2 - config.topCross.width / 2;
-  ctx.lineTo(x, y);
+    const rectCount = 5;
+    const rectWidth = 2;
+    const rectHeight = 8;
+    for (let i = 0; i < rectCount; i += 1) {
+      ctx.rect(
+        x -
+          crownWidth / 2 +
+          ((crownWidth - rectWidth * rectCount) / (rectCount - 1) + rectWidth) *
+            i,
+        y - crownHeight,
+        rectWidth,
+        -rectHeight
+      );
+      ctx.fill();
+    }
+    // ctx.arc(
+    //   config.base.x,
+    //   config.base.y - figureHeight - 9,
+    //   20,
+    //   0,
+    //   2 * Math.PI
+    // );
+  }
 
-  y -= config.topCross.height * mirror;
-  ctx.lineTo(x, y);
-
-  x -= config.shift / 2 - config.topCross.width / 2;
-  ctx.lineTo(x, y);
-
-  ctx.arc(
-    x - (config.shift * mirror) / 2,
-    y - (config.botCross.distance * mirror) / 2,
-    config.radius,
-    (Math.PI / 2) * mirror - angle,
-    (-Math.PI / 2) * mirror + angle,
-    true
-  );
-  y -= config.botCross.distance * mirror;
-  ctx.lineTo(x, y);
-
-  x -= config.botCross.width / 2 - config.shift / 2;
-  ctx.lineTo(x, y);
-
-  y -= config.botCross.height * mirror;
-  ctx.lineTo(x, y);
-
-  x -= config.thirdSeg.widthTop / 2 - config.botCross.width / 2;
-  ctx.lineTo(x, y);
-
-  x -= (config.thirdSeg.widthBottom - config.thirdSeg.widthTop) / 2;
-  y -= config.thirdSeg.height * mirror;
-  ctx.lineTo(x, y);
-
-  x -= (config.secSeg.width - config.thirdSeg.widthBottom) / 2;
-  ctx.lineTo(x, y);
-
-  y -= config.secSeg.height * mirror;
-  ctx.lineTo(x, y);
-
-  x -= config.base.width / 2 - config.secSeg.width / 2;
-  ctx.lineTo(x, y);
-
-  y -= config.base.height * mirror;
-  ctx.lineTo(x, y);
-
-  x += config.base.width / 2;
-  ctx.lineTo(x, y);
-
+  drawHalfFigure(1);
+  drawHalfFigure(-1);
   ctx.fill();
-  // ctx.closePath();
+
+  drawCrown();
   ctx.stroke();
 }
+
 drawQueen();
